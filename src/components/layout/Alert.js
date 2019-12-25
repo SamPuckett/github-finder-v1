5833d82d07e9fd2b44e9857117cddba2367e5c60
Samuel Ruiz <samuelpuckett77@gmail.com>
2019-12-25 10:19:09 -0800

    L33: Alert Context Workflow
    - create a seperate context for alerts which held who pieces of information, alert, and setAlert
    - went throught the workflow of creating a new context once we already have a context set up, which is a lot easier than setting up our first context in a project

--- src/components/layout/Alert.js
+++ src/components/layout/Alert.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
+import AlertContext from "../../context/alert/alertContext";
 
-const Alert = ({ alert }) => {
+const Alert = () => {
+  // initialize our context
+  const alertContext = useContext(AlertContext);
+
+  // destructure our alert variable from our context
+  const { alert } = alertContext;
+
   return (
     alert !== null && (
       <div className={`alert alert-${alert.type}`}>
