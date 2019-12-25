0f1ad8e4ce0688148f5a07df9d7ed4851c08b9a4
Samuel Ruiz <samuelpuckett77@gmail.com>
2019-12-24 20:29:17 -0800

    L31: Pt 2
    - finish moving getUser form App.js to GithubState so that we can access the method through context and we don't have to pass it as props to the <User /> component.
    - We did some clean up as well, removed prop types, removed useState declarations, added method to githubState, initialized our githubContext in App.js, destructured our { loading, user, getUser } from github Context, and removed the loading, user, and getUser props from being destuctured

--- src/context/github/githubReducer.js
+++ src/context/github/githubReducer.js
@@ -19,7 +19,13 @@ export default (state, action) => {
       return {
         ...state,
         users: [],
-        loadin: false
+        loading: false
+      };
+    case GET_USER:
+      return {
+        ...state,
+        user: action.payload,
+        loading: false
       };
     case SET_LOADING:
       return {
